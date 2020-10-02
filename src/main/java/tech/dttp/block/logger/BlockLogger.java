f21461da4ee79f0f2a30864f52a09c67eeb7c460
yitzy299 <hugocarter@hotmail.co.uk>
2020-10-02 22:35:29 +0100

    Stuff
    Version bump to 1.16.3
    Added SQLite as dependency
    Other stuff

--- src/main/java/tech/dttp/block/logger/BlockLogger.java
+++ src/main/java/tech/dttp/block/logger/BlockLogger.java
@@ -3,22 +3,19 @@ package tech.dttp.block.logger;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import tech.dttp.block.logger.events.PlaceBlockCallback;
-
-import tech.dttp.block.logger.save.sql.Write;
-import tech.dttp.block.logger.save.txt.ReadConfig;
-import tech.dttp.block.logger.save.txt.TxtWrite;
-
 import net.minecraft.util.ActionResult;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.world.BlockEvent;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import tech.dttp.block.logger.save.sql.Write;
+import tech.dttp.block.logger.save.txt.ReadConfig;
+import tech.dttp.block.logger.save.txt.TxtWrite;
 
 import java.io.IOException;
 
@@ -43,13 +40,6 @@ public class BlockLogger implements ModInitializer {
             }
         });
         //todo: Block placement pos via hitresult
-        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-            return ActionResult.PASS;
-        });
-        PlaceBlockCallback.EVENT.register((player, world, hand, hitResult, pos) -> {
-            System.out.println("HALLO");
-            return ActionResult.FAIL;
-        });
     }
 }
 
