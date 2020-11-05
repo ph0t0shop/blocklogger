e67d0c32aa1483c49c425e7b73a1030388bf6dd5
yitzy299 <hugocarter@hotmail.co.uk>
2020-11-05 18:26:10 +0000

    Update BlockPlaceMixin.java
    Cleaned up code and optimised

--- src/main/java/tech/dttp/block/logger/mixin/BlockPlaceMixin.java
+++ src/main/java/tech/dttp/block/logger/mixin/BlockPlaceMixin.java
@@ -5,16 +5,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
 import tech.dttp.block.logger.save.sql.DbConn;
 import tech.dttp.block.logger.util.LoggedEventType;
 
@@ -24,21 +17,8 @@ public abstract class BlockPlaceMixin extends Item {
     super(settings);
   }
 
-  /**
-   * Log item placement hook
-   */
-  @Inject(
-    at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/item/ItemPlacementContext;getBlockPos()Lnet/minecraft/util/math/BlockPos;"
-    ),
-    method = "place"
-  )
-  public void place(ItemPlacementContext context, CallbackInfoReturnable<Boolean> info) {
-      BlockPos pos = context.getBlockPos();
-      PlayerEntity player = context.getPlayer();
-      World world = context.getWorld();
-      BlockState state = world.getBlockState(pos);
-      DbConn.writeInteractions(pos.getX(), pos.getY(), pos.getZ(), state, player, LoggedEventType.placed);
+  @Inject(at = @At(value = "INVOKE",target = "Lnet/minecraft/item/ItemPlacementContext;getBlockPos()Lnet/minecraft/util/math/BlockPos;"),method = "place")
+  public void place(ItemPlacementContext c, CallbackInfoReturnable<Boolean> info) {
+        DbConn.writeInteractions(c.getBlockPos().getX(), c.getBlockPos().getY(), c.getBlockPos().getZ(), c.getWorld().getBlockState(c.getBlockPos()), c.getPlayer(), LoggedEventType.placed);
   }
 }
