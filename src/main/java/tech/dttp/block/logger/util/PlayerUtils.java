42feaa66e9190848481d11b5fe6bc9d2542f955b
yitzy299 <hugocarter@hotmail.co.uk>
2020-11-18 19:32:16 +0000

    Fix some minor issues and fully implemented transactions
    - Container transactions now log to the DB, with no breaking changes
    - Added framework for inspector however it cannot be enabled (Waiting for command)

--- src/main/java/tech/dttp/block/logger/util/PlayerUtils.java
+++ src/main/java/tech/dttp/block/logger/util/PlayerUtils.java
@@ -1,8 +1,6 @@
 package tech.dttp.block.logger.util;
 
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.World;
 
 public class PlayerUtils {
     public static String getPlayerDimension(PlayerEntity player) {
