22a3e0d2af13db42b5f0b70946dabfe34a23aae6
yitzy299 <hugocarter@hotmail.co.uk>
2020-11-20 21:14:01 +0000

    Create BucketItemMixin class
    ^

---
+++ src/main/java/tech/dttp/block/logger/mixin/BucketItemMixin.java
@@ -0,0 +1,19 @@
+package tech.dttp.block.logger.mixin;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BucketItem;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(BucketItem.class)
+public class BucketItemMixin {
+    
+}
